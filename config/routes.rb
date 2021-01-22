725a254535d56978253361273ee89cedcae92f51
Ganthology <gbk1386@gmail.com>
2021-01-22 23:01:24 +0800

    Add #current_user and #logged_in to application helper, edit nav bar links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
